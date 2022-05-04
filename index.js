2ec9d98f6f4eb85f7d214f74c47e9940c531d29e
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2022-05-05 00:02:24 +0530

    fabric server setup

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+
+const registerUser = require('./utils/registerUser');
+const pushData = require('./utils/pushData');
+
+const port = 3000;
+const organisationNumber = 1;
+const organisationName = "A";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from the server!");
+});
+
+app.post("/register", async (req, res) => {
+    const { userId } = req.body;
+    if (!userId)
+        res.json({ 
+            success: false,
+            msg: "UserId is required!"
+         });
+    try{
+        const msg = await registerUser({
+            organisationNumber,
+            userId
+        });
+        res.json({
+            success: true,
+            msg
+        })
+    }catch(err){
+        res.json({
+            success: false,
+            msg: err
+        })
+    }
+});
+
+app.post("/push", async (req, res) => {
+    const { userId, data} = req.body;
+    if (!userId || !data)
+        res.json({ 
+            success: false,
+            msg: "UserId & data is required!"
+         });
+    try{
+        const msg = await pushData({
+            organisationNumber,
+            organisationName,
+            userId,
+            data
+        });
+        res.json({
+            success: true,
+            msg
+        })
+    }catch(err){
+        res.json({
+            success: false,
+            msg: err
+        })
+    }
+});
+
+
+app.listen(port, (err) => {
+    if (err)
+        console.log("Error while starting the server: ", err);
+    console.log("Server is up and running on port", port);
+});
